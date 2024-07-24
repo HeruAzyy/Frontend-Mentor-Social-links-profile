e770409ac925d078e8b8639b308153b69d9b0811
Heru Azy <heruazyp@gmail.com>
2024-07-24 15:47:27 +0700

    Initial Project

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['*.html'],
+	theme: {
+		fontFamily: {
+			inter: ['Inter']
+		},
+		container: {
+			center: true
+		},
+		extend: {}
+	},
+	plugins: []
+}
